e4d16be96630fce35819c122cd2c2b3d1bbdfb2a
Yasini V <yasinivijayakumar021@gmail.com>
2026-06-03 23:55:03 +0530

    Fix dashboard css import

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 import Navbar from "../components/Navbar";
-import "../pages/dashboard.css";
+import "../pages/Dashboard.css";
 import UrlForm from "../components/UrlForm";
 import UrlTable from "../components/UrlTable";
 import { FiLink, FiBarChart2 } from "react-icons/fi";
